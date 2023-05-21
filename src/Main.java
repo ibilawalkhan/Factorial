fde583d55b7ab4bd28d068c0fa187ddb0dcb2f7e
ibilawalkhan <kbilawal84437@gmail.com>
2023-05-22 00:05:27 +0500

    Commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a number: ");
+        int num = input.nextInt();
+
+        int fact = 1;
+
+        for(int i=1; i<=num; i++) {
+            fact = fact * i;
+        }
+
+        System.out.println("Factorial of the given number is: " +fact);
+    }
+}
